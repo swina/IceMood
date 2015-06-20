2832b12d16a545e284b0b25e5991b41e64e81155
swina <swina.allen@gmail.com>
2015-06-21 01:11:33 +0300

    Update icegenerator.php

--- application/config/icegenerator.php
+++ application/config/icegenerator.php
@@ -1,13 +1,13 @@
 <?php
 
-$config["icegen_ip"] = "178.239.177.98";
-$config["icegen_port"] = "8080";
+$config["icegen_ip"] = "server_ip";
+$config["icegen_port"] = "icecast_port";
 $config["icegen_mount"] = "/radio";
 $config["icegen_user"] = "source";
-$config["icegen_password"] = "moodgiverradio";
+$config["icegen_password"] = "icecast password";
 $config["icegen_format"] = "1";
 $config["icegen_type"] = "m3u:";
-$config["icegen_name"] = "ItalianMood Radio";
+$config["icegen_name"] = "Your Radio Name";
 $config["icegen_genre"] = "Only good music";
-$config["icegen_description"] = "ItalianMood Radio - Born To Share";
-$config["icegen_timezone"] = "Europe/Istanbul";
+$config["icegen_description"] = "Your radio description";
+$config["icegen_timezone"] = "Europe/Rome";
