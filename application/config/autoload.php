1e46413f227fde47f12d5467eb374ae5f9eae307
swina <swina.allen@gmail.com>
2015-06-22 00:38:10 +0300

    No database library load
    Do not load database setting

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -60,7 +60,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-$autoload['libraries'] = array('database', 'session','template'  );
+$autoload['libraries'] = array('session','template'  );
 
 
 /*
